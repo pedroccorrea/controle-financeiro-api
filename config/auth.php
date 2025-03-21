11047c88b8ea9037964b0d4c1cf381e598ea46b9
Pedro  C. Correa <pedrocorrea.dev@gmail.com>
2025-03-21 06:16:30 -0300

    Iniciando a criação da autenticação com JWT

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => 'api',
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -39,6 +39,12 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
+        ],
+        
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+            'has' => false,
         ],
     ],
 
